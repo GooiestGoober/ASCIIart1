62b58b90d1e2e8ce84e3dc7aa7ab348c7a957184
GooiestGoober <octavio.cervera@allsaintshigh.org>
2025-08-24 15:01:03 -0500

    the little guy

---
+++ Main.java
@@ -0,0 +1,23 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("     _______");
+        System.out.println("   /         \\");
+        System.out.println("  |  0    0   |");
+        System.out.println("  |    <      |");
+        System.out.println("  |  _____    |");
+        System.out.println("   \\_________/");
+        System.out.println("        |");
+        System.out.println("  ______|______");
+        System.out.println("        |");
+        System.out.println("        |");
+        System.out.println("        |");
+        System.out.println("        |");
+        System.out.println("       / \\");
+        System.out.println("      /   \\");
+        System.out.println("     /     \\");
+        System.out.println(" ___/       \\___");
+
+    }
+}
